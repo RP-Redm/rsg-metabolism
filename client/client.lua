e6e0a027c9cc265223357ac2693a96f49ea19d04
RexShack <rexshackuk@gmail.com>
2023-01-15 23:26:48 +0000

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -1,6 +1,18 @@
 local RSGCore = exports['rsg-core']:GetCoreObject()
+local isLoggedIn = false
+local PlayerData = {}
 local roundtemp = 0
 local cashAmount = 0
+
+AddEventHandler('RSGCore:Client:OnPlayerLoaded', function()
+    isLoggedIn = true
+    PlayerData = RSGCore.Functions.GetPlayerData()
+end)
+
+RegisterNetEvent('RSGCore:Client:OnPlayerUnload', function()
+    isLoggedIn = false
+    PlayerData = {}
+end)
 
 local ClothesCats = {
     0x9925C067, --hat
@@ -18,37 +30,42 @@ local ClothesCats = {
 Citizen.CreateThread(function()
     while true do
         Wait(1500)
-        local ped = PlayerPedId()
-        local coords = GetEntityCoords(ped)
-        local temp = Citizen.InvokeNative(0xB98B78C3768AF6E0,coords.x,coords.y,coords.z)
-        roundtemp = foo(temp)
-        
-        for k,v in pairs(ClothesCats) do
-            local IsWearingClothes = Citizen.InvokeNative(0xFB4891BD7578CDC1 ,PlayerPedId(), v)
-            if IsWearingClothes then 
-                roundtemp = roundtemp + 1
+        if isLoggedIn then
+            local ped = PlayerPedId()
+            local coords = GetEntityCoords(ped)
+            local temp = Citizen.InvokeNative(0xB98B78C3768AF6E0,coords.x,coords.y,coords.z)
+            roundtemp = foo(temp)
+            
+            for k,v in pairs(ClothesCats) do
+                local IsWearingClothes = Citizen.InvokeNative(0xFB4891BD7578CDC1 ,PlayerPedId(), v)
+                if IsWearingClothes then 
+                    roundtemp = roundtemp + 1
+                end
             end
+            RSGCore.Functions.GetPlayerData(function(PlayerData)
+                cashAmount = PlayerData.money['cash']
+            end)
         end
-        RSGCore.Functions.GetPlayerData(function(PlayerData)
-            cashAmount = PlayerData.money['cash']
-        end)
     end
 end)
 
 Citizen.CreateThread(function()
     while true do
         Wait(5)
-        -- get ID and display
-        DrawTxt("ID:"..tonumber(GetPlayerServerId(PlayerId())), 0.02, 0.95, 0.4, 0.4, true, 255, 255, 255, 255, true, 10000)
-        -- get temp and display
-        if tonumber(roundtemp) <= 0 then
-            DrawTxt(""..tonumber(roundtemp).."°c", 0.06, 0.95, 0.4, 0.4, true, 50, 72, 255, 255, true, 10000)
-        elseif tonumber(roundtemp) < 3 then 
-            DrawTxt(""..tonumber(roundtemp).."°c", 0.06, 0.95, 0.4, 0.4, true, 3, 231, 255, 255, true, 10000)
-        elseif tonumber(roundtemp) then
-            DrawTxt(""..tonumber(roundtemp).."°c", 0.06, 0.95, 0.4, 0.4, true, 255, 255, 255, 255, true, 10000)
+        if isLoggedIn then
+            -- get ID and display
+            DrawTxt("ID:"..tonumber(GetPlayerServerId(PlayerId())), 0.02, 0.95, 0.4, 0.4, true, 255, 255, 255, 255, true, 10000)
+            -- get temp and display
+            if tonumber(roundtemp) <= 0 then
+                DrawTxt(""..tonumber(roundtemp).."°c", 0.06, 0.95, 0.4, 0.4, true, 50, 72, 255, 255, true, 10000)
+            elseif tonumber(roundtemp) < 3 then 
+                DrawTxt(""..tonumber(roundtemp).."°c", 0.06, 0.95, 0.4, 0.4, true, 3, 231, 255, 255, true, 10000)
+            elseif tonumber(roundtemp) then
+                DrawTxt(""..tonumber(roundtemp).."°c", 0.06, 0.95, 0.4, 0.4, true, 255, 255, 255, 255, true, 10000)
+            end
+            -- get cash and display
+            DrawTxt("$:"..string.format("%.2f", cashAmount), 0.12, 0.95, 0.4, 0.4, true, 255, 255, 255, 255, true, 10000)
         end
-        DrawTxt("$:"..string.format("%.2f", cashAmount), 0.12, 0.95, 0.4, 0.4, true, 255, 255, 255, 255, true, 10000)
     end
 end)
 
@@ -56,25 +73,27 @@ end)
 Citizen.CreateThread(function()
     while true do
         Wait(5000)
-        ped = PlayerPedId()
-        health = GetEntityHealth(ped)
-        coords = GetEntityCoords(ped)
-        if tonumber(roundtemp) <= -8 then
-            SetEntityHealth(ped,health  - 5)
-        elseif tonumber(roundtemp) <= -6 then
-            SetEntityHealth(ped,health  - 2)
-        elseif tonumber(roundtemp) <= -4 then
-            SetEntityHealth(ped,health  - 1)
-        end
-        if health > 0 and health < 50 and tonumber(roundtemp) > 0 then 
-            SetEntityHealth(ped,health  - 1)
-            PlayPain(ped, 9, 1, true, true)
-            Citizen.InvokeNative(0x4102732DF6B4005F, "MP_Downed", 0, true) -- AnimpostfxPlay
-        else
-            if Citizen.InvokeNative(0x4A123E85D7C4CA0B, "MP_Downed") then -- AnimpostfxIsRunning
-                Citizen.InvokeNative(0xB4FD7446BAB2F394, "MP_Downed") -- AnimpostfxStop
-            end
-        end
+		if isLoggedIn then
+			ped = PlayerPedId()
+			health = GetEntityHealth(ped)
+			coords = GetEntityCoords(ped)
+			if tonumber(roundtemp) <= -8 then
+				SetEntityHealth(ped,health  - 5)
+			elseif tonumber(roundtemp) <= -6 then
+				SetEntityHealth(ped,health  - 2)
+			elseif tonumber(roundtemp) <= -4 then
+				SetEntityHealth(ped,health  - 1)
+			end
+			if health > 0 and health < 50 and tonumber(roundtemp) > 0 then 
+				SetEntityHealth(ped,health  - 1)
+				PlayPain(ped, 9, 1, true, true)
+				Citizen.InvokeNative(0x4102732DF6B4005F, "MP_Downed", 0, true) -- AnimpostfxPlay
+			else
+				if Citizen.InvokeNative(0x4A123E85D7C4CA0B, "MP_Downed") then -- AnimpostfxIsRunning
+					Citizen.InvokeNative(0xB4FD7446BAB2F394, "MP_Downed") -- AnimpostfxStop
+				end
+			end
+		end
     end
 end)
 
