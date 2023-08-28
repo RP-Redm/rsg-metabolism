668c1c4d7e4b8aa24dc88e66bfc3cdb282c5dca7
RexshackGaming <rexshackuk@gmail.com>
2023-08-28 12:30:42 +0100

    remove infohud

--- config.lua
+++ config.lua
@@ -1,3 +1,13 @@
 Config = {}
 
-Config.ShowInfoHud = true
+Config.ClothesCats = {
+    0x9925C067, --hat
+    0x2026C46D, --shirt
+    0x1D4C528A, -- pants
+    0x777EC6EF, -- boots 
+    0xE06D30CE, -- coat
+    0x662AC34, --open coat
+    0xEABE0032, --gloves 
+    0x485EE834, --vest
+    0xAF14310B, -- poncho
+}
